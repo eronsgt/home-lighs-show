6524b2062cd225c49973b7bc7f95acbd0f38c727
eronsgt <drumhead26@gmail.com>
2014-06-10 22:14:17 +0100

    Update core.js
    added particle effect
    small fixes

--- core.js
+++ core.js
@@ -4,16 +4,33 @@
 var osc = require('node-osc');
 var oscServer = new osc.Server(8000, '0.0.0.0');
 var OPC = new require('./opc');
-// var model = OPC.loadModel(process.argv[2] || '/home/pi/fadecandy/examples/layouts/strip230n.json');
+var model = OPC.loadModel(process.argv[2] || '/home/pi/fadecandy/examples/layouts/strip230n.json');  // layout generated using https://github.com/scanlime/fadecandy/tree/master/examples/layouts
 var client = new OPC('localhost', 7890);
  
- 
+
+ // effect 1 (strip fade) 
 var red1 = 255;
 var green1 = 255;
 var blue1 = 255;
 var fall = 0;
+
+// strobe
 var strobeON = 0;
 var sdelay = 0;
+
+// effect 2 (particle) source: https://github.com/scanlime/fadecandy/blob/master/examples/node/particle_touchosc.js
+var stateAngle1 = 0;
+var stateAngle2 = 0;
+var hue = 0.3;
+var hueShift = 0.8;
+var saturation = 0.5;
+var brightness = 0.5;
+var falloff = 0.5;
+var rate1 = 0.1;
+var rate2 = 0.2;
+var particleON = 0;
+
+
  
  
 oscServer.on('message', function (msg, rinfo) {
@@ -22,28 +39,90 @@ oscServer.on('message', function (msg, rinfo) {
     console.log(msg);
  
  
+// strobe 
+if (msg[0] == '/5/STROB') strobeON = msg[1];
+if (msg[0] == '/5/SDELAY') sdelay = msg[1];
  
+// effect 1 (strip fade) + strobe
 if (msg[0] == '/1/RED') red1 = msg[1];
 if (msg[0] == '/1/GREEN') green1 = msg[1];
 if (msg[0] == '/1/BLUE') blue1 = msg[1];
 if (msg[0] == '/1/FALLOFF') fall = msg[1];
-if (msg[0] == '/1/STROB') strobeON = msg[1];
-if (msg[0] == '/1/SDELAY') sdelay = msg[1];
+
+// effect 2 (particle)
+if (msg[0] == '/4/hue') hue = msg[1];
+if (msg[0] == '/4/hueShift') hueShift = msg[1];
+if (msg[0] == '/4/saturation') saturation = msg[1];
+if (msg[0] == '/4/falloff2') falloff = msg[1];
+if (msg[0] == '/4/brightness') brightness = msg[1];
+if (msg[0] == '/4/particleON') particleON = msg[1];
+if (msg[0] == '/4/xy1') { // XY pad, oscillator rates
+        rate1 = msg[1];
+        rate2 = msg[2];
+    }
 });
  
  
 function draw() {
     if (strobeON == 1) { // Button Strobe is pressed
-        console.log('do strobe');
+     //   console.log('do strobe');
         doStrobe(function(){
             setTimeout(draw,30); // repeat
         });
-    } else {
+        
+    }   else if  (particleON == 1) { // Button Particle is toggled ON
+      //  console.log('particle ON');
+        doParticles(function(){
+            setTimeout(draw,30); // repeat
+        });     
+    }   else {
         doFall(function(){
             setTimeout(draw,30); // repeat
         });
     }
 }
+ 
+ 
+ 
+ 
+ function doParticles(callback) {
+
+    var numParticles = 230;
+    var particles = [];
+
+    var trail = 100.0;
+    var scaledRate1 = (rate1 - 0.4) * 0.4;
+    var scaledRate2 = (rate2 - 0.4) * 0.4;
+    stateAngle1 += scaledRate1;
+    stateAngle2 += scaledRate2;
+
+    for (var i = 0; i < numParticles; i++) {
+        var s = i / numParticles;
+
+        // Local state angles for this particle
+        var lsa1 = stateAngle1 + scaledRate1 * trail * s;
+        var lsa2 = stateAngle2 + scaledRate2 * trail * s;
+
+        // Compute position, this gives us the shape of our effect.
+        var radius = 0.1 + 2.0 * s;
+        var x = radius * Math.cos(lsa1);
+        var y = radius * Math.sin(lsa2);
+
+        particles[i] = {
+            point: [x, 0, y],
+            intensity: brightness * (1 - s),
+            falloff: falloff * 175,
+            color: OPC.hsv( hue + s * 2.0 * (hueShift - 0.5), saturation, 0.5)
+        };
+    }
+
+    client.mapParticles(particles, model);
+    callback();
+}
+ 
+ 
+ 
+ 
  
 function doFall(callback) {
     var millis = new Date().getTime();
